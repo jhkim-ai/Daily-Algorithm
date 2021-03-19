aa0113048ab90ec22ee4f10b44d0ab8df151e1bd
김재현 <kimjaehyun9409@gmail.com>
2021-03-19 15:44:12 +0900

    21.03.19 dp 파일정리

---
+++ src/Category/Dynamic_Programming/BOJ1904_S3_01타일.java
@@ -0,0 +1,27 @@
+package Category.Dynamic_Programming;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+public class BOJ1904_S3_01타일 {
+
+    static int N;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception{
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new StringReader(input));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <1000001 ; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%15746;
+        }
+
+        System.out.println(dp[111]);
+    }
+
+    static String input = "4";
+}
