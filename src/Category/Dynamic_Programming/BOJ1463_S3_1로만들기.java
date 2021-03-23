cea8e02fd6c9bc009f7e982f7fd694533749ca93
김재현 <kimjaehyun9409@gmail.com>
2021-03-23 17:25:01 +0900

    21.03.21 dp 1로 만들기 주석 추가

--- src/Category/Dynamic_Programming/BOJ1463_S3_1로만들기.java
+++ src/Category/Dynamic_Programming/BOJ1463_S3_1로만들기.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class BOJ1463_S3_1로만들기 {
 
     static int N;
-    static int[] dp;
+    static int[] dp; // memoization
 
     public static void main(String[] args) throws Exception{
         // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,10 +16,15 @@ public class BOJ1463_S3_1로만들기 {
         dp = new int[N+1];
 
         // Bottom - Up (for 문 이용)
+        // ex. N = 6
+        // 3가지 경우의 수
+        // (1) N - 1
+        // (2) N / 2
+        // (3) N / 3
         for (int i = 2; i < N+1; i++) {
-            dp[i] = dp[i-1] + 1;
-            if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1);
-            if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+            dp[i] = dp[i-1] + 1;    // (1)
+            if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1); // (2)
+            if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1); // (3)
         }
 
         System.out.println(dp[N]);
