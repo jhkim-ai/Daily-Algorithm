ab53452965821dc08f2e77a6beb2f4d97288ea8d
김재현 <kimjaehyun9409@gmail.com>
2021-04-21 02:04:18 +0900

    21.04.21 문자열, 압축(미해결)

--- src/StepByStep/day210423/BOJ1662_압축.java
+++ src/StepByStep/day210423/BOJ1662_압축.java
@@ -4,9 +4,26 @@ import java.util.*;
 import java.io.*;
 
 public class BOJ1662_압축 {
-    public static void main(String[] args) {
+
+    static String str;
+
+    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new StringReader(input));
+        str = br.readLine();
+
+        // Idea. 길이를 계산
+
+        // 주의!. String으로 접근했을 때, Worst Case로에 의하여 메모리 초과가 난다.
+        // Worst Case : 9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(111))))))))))))))))))))))))
+
 
     }
 
-    static String input = "33(562(71(9)))";
+    //    static String input = "33(56(2)1(719))2(2)";
+//    static String input = "33(562(71(9)))";
+//        static String input = "6(22)122";
+//        static String input = "3(3(3(2(2)2(2))))1(1)1(1)";
+//    static String input = "3333";
+    static String input = "9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(111))))))))))))))))))))))))";
 }
