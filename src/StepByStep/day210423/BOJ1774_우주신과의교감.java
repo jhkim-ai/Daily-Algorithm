7d90934451d21754668849c0ded6d81c78c5a5b7
김재현 <kimjaehyun9409@gmail.com>
2021-04-23 00:40:56 +0900

    21.04.23 MST, 우신과의 교감

--- src/StepByStep/day210423/BOJ1774_우주신과의교감.java
+++ src/StepByStep/day210423/BOJ1774_우주신과의교감.java
@@ -6,8 +6,8 @@ import java.io.*;
 public class BOJ1774_우주신과의교감 {
 
     static int N, M;
-    static List<Point> list;
-    static List<Point> distance;
+    static List<Point> locList;
+    static List<Edge> edgeList;
     static int[] root;
     static int[] rank;
     static double ans;
@@ -18,49 +18,55 @@ public class BOJ1774_우주신과의교감 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        list = new ArrayList<>();
-        distance = new ArrayList<>();
+        locList = new ArrayList<>();
+        edgeList = new ArrayList<>();
 
         for (int i = 1; i <= N; ++i) {
             st = new StringTokenizer(br.readLine(), " ");
-            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i));
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            locList.add(new Point(y, x, i));
         }
-        for(int i = 0; i<M; ++i){
+
+        // 0. 집합 초기화
+        makeSet();
+
+        // 0-1. 제시된 간선 M개를 Union
+        for (int i = 0; i < M; ++i) {
             st = new StringTokenizer(br.readLine(), " ");
             union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        // 모든 점에 대하여 거리를 구한다.
-        for (int i = 0; i < list.size(); ++i) {
-            for (int j = i+1; j < list.size(); ++j){
-                Point p = list.get(i);
-                Point q = list.get(j);
-                Point info = new Point();
-                info.num1 = p.num1;
-                info.num2 = q.num1;
-                info.getDistance(p, q);
-                distance.add(info);
+        // Idea. MST 를 만들자 (Kruskal)
+
+        // 1. 모든 점에 대하여 거리를 구한다.
+        for (int i = 0; i < locList.size(); ++i) {
+            for (int j = i + 1; j < locList.size(); ++j) {
+                Point p = locList.get(i);
+                Point q = locList.get(j);
+                double weight = setDistance(p, q);
+                Edge edge = new Edge(p.num, q.num, weight);
+                edgeList.add(edge);
             }
         }
 
-        // 0. kruskal 알고리즘을 위한 거리에 대한 오름차순 정렬
-        Collections.sort(distance);
-        // 1. 집합 초기화
-        makeSet();
-        // 2. 가장 작은 edge 부터 MST를 완성해 나간다.
-        for(Point line : distance){
-            if(N == M-1)
-                break;
-            if(!union(line.num1, line.num2)) break;
-            ans += line.distance;
-        }
+        // 2. 거리에 대한 오름차순 정렬
+        Collections.sort(edgeList);
 
-        System.out.printf("%.2f",ans);
+        // 3. 가장 작은 edge 부터 MST를 완성해 나간다.
+        for (Edge e : edgeList) {
+            if (N - 1 == M)
+                break;
+            if (!union(e.start, e.end)) continue;
+            ans += e.weight;
+            ++M;
+        }
+        System.out.printf("%.2f", ans);
     }
 
     static void makeSet() {
-        root = new int[list.size()+1];
-        rank = new int[list.size()+1];
+        root = new int[N + 1];
+        rank = new int[N + 1];
         for (int i = 0; i < root.length; i++) {
             root[i] = i;
         }
@@ -78,47 +84,52 @@ public class BOJ1774_우주신과의교감 {
 
         if (a == b) return false;
 
-        if(rank[a] < rank[b])
+        if (rank[a] < rank[b])
             root[a] = b;
         else {
             root[b] = a;
-            if(rank[a] == rank[b])
+            if (rank[a] == rank[b])
                 rank[a]++;
         }
         return true;
     }
 
-    static double getDistance(Point p, Point q){
-        int width = (p.x - q.x) * (p.x - q.x);
-        int height = (p.y - q.y) * (p.y - q.y);
+    static double setDistance(Point p, Point q) {
 
-        return Math.sqrt(width + height);
+        return Math.sqrt(Math.pow(p.x - q.x, 2) + Math.pow(p.y - q.y, 2));
     }
-    static class Point implements Comparable<Point>{
+
+    static class Point {
         int y;
         int x;
-        int num1;
-        int num2;
-        double distance;
+        int num;
 
-        public Point(){
-
-        }
-        public Point(int y, int x, int num1) {
+        public Point(int y, int x, int num) {
             this.y = y;
             this.x = x;
-            this.num1 = num1;
+            this.num = num;
         }
-        void getDistance(Point p, Point q) {
-            int width = (p.x - q.x) * (p.x - q.x);
-            int height = (p.y - q.y) * (p.y - q.y);
+    }
 
-            this.distance = Math.sqrt(width + height);
+    static class Edge implements Comparable<Edge> {
+        int start;
+        int end;
+        double weight;
+
+        public Edge(int start, int end, double weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
         }
 
         @Override
-        public int compareTo(Point p){
-            return Double.compare(this.distance, p.distance);
+        public int compareTo(Edge e) {
+            return Double.compare(this.weight, e.weight);
+        }
+
+        @Override
+        public String toString() {
+            return weight + "";
         }
     }
 }
