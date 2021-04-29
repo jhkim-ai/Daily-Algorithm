5d2970c9f01f8cca2609fceb97ffeb8ca2ca15e1
김재현 <kimjaehyun9409@gmail.com>
2021-04-29 23:22:56 +0900

    21.04.29 DP, BOJ_2687 : 성냥개비

--- src/StepByStep/day210430/BOJ2687_성냥개비.java
+++ src/StepByStep/day210430/BOJ2687_성냥개비.java
@@ -6,6 +6,7 @@ import java.io.*;
 public class BOJ2687_성냥개비 {
 
     static final int[] arr = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+    static long[] dp;
 
     static int T, N;
     static int ansMin, ansMax;
@@ -13,72 +14,70 @@ public class BOJ2687_성냥개비 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder ans = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+
+        // Idea. DP
+        // 1. 최솟값은 dp를 이용해서 푼다.
+        initDp();
+        // 2. 최댓값은 getMaxValue()를 이용
 
         T = Integer.parseInt(br.readLine());
         for (int t = 1; t <= T; ++t) {
             N = Integer.parseInt(br.readLine());
-            ansMin = Integer.MAX_VALUE;
-            ansMax = Integer.MIN_VALUE;
-
-            // 1. 부분집합을 구한다.
-            powerSet();
-            ans.append(ansMin).append(" ").append(ansMax).append("\n");
+            long min = dp[N];
+            String max = getMaxValue(N);
+            sb.append(String.format("%d %s\n", min, max));
         }
-        System.out.println(ans);
+
+        System.out.println(sb);
     }
 
-    public static void powerSet() {
-        list = new ArrayList<>();
+    public static void initDp() {
+        dp = new long[101];
+        Arrays.fill(dp, Long.MAX_VALUE);
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 7;
+        dp[4] = 4;
+        dp[5] = 2;
+        dp[6] = 6;  // 처음에만 6 나머지는 0 이다.
+        dp[7] = 8;
+        dp[8] = 10; // dp[1] + dp[7] 에서 예외 발생 (성냥개비가 1개 남음)
 
-        // bit masking 부분집합
-        for (int i = 1; i < (1 << arr.length); ++i) { // 2^10(1024) 개의 부분집합 중 공집합을 제외
-            int sum = 0;
-            list.clear();
-            for (int j = 0; j < arr.length; ++j) {
-                if ((i & (1 << j)) > 0) {
-                    list.add(arr[j]);
-                }
+        for (int i = 9; i <= 100; ++i) {
+            // 2 ~ 7까지 하는 이유 : 일의 자리를 만들기 위해선 최소 2개(1) ~ 최대 7개(8)까지 가능하다.
+            for (int j = 2; j <= 7; ++j) {
+                String now = "";
+                if (j == 6)
+                    now = dp[i - j] + "0";
+                else
+                    now = dp[i - j] + Long.toString(dp[j]);
+                dp[i] = Math.min(dp[i], Long.parseLong(now));
             }
-
-            // 가지치기
-            for (int num : list) {
-                sum += arr[num];
-            }
-            if (sum != N) continue;
-
-            // 2. 선택된 부분 집합을 조합한다.
-            sorting();
         }
     }
 
-    static void sorting() {
+    public static String getMaxValue(int n) {
         StringBuilder sb = new StringBuilder();
-        Collections.sort(list);
 
-        // 최솟값
-        int inter = -1;
-        if(list.get(0) == 0){
-            for(int i = 0; i < list.size(); ++i){
-                if(list.get(i) == 0) continue;
-                inter = i;
-                break;
+        // 몫(digit)만큼 길이(자릿수)가 생긴다.
+        int len = n / 2;
+
+        // 짝수면, 1111...
+        if (n % 2 == 0) {
+            for (int i = 1; i <= len; ++i) {
+                sb.append("1");
+            }
+        }
+        // 홀수면, 7111...
+        else {
+            sb.append("7");
+            for (int i = 2; i <= len; ++i) {
+                sb.append("1");
             }
         }
 
-        if(inter != -1)
-            sb.append(list.get(inter));
-        for(int i = 0; i < list.size(); ++i){
-            if(i == inter) continue;
-            sb.append(list.get(i));
-        }
-        ansMin = Math.min(Integer.parseInt(sb.toString()), ansMin);
-
-        sb.setLength(0);    // 가장 빠른 초기화라 한다.
-        // 최대값
-        for (int i = list.size() - 1; i >= 0; --i) {
-            sb.append(list.get(i));
-        }
-        ansMax = Math.max(Integer.parseInt(sb.toString()), ansMax);
+        return sb.toString();
     }
 }
