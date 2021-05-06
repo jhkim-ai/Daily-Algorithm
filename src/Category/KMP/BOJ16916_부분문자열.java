55d056f444976155d05ede7af5ae889771fe3bbd
김재현 <kimjaehyun9409@gmail.com>
2021-05-06 13:17:33 +0900

    21.05.06 임시 저장

---
+++ src/Category/KMP/BOJ16916_부분문자열.java
@@ -0,0 +1,50 @@
+package Category.KMP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ16916_부분문자열 {
+
+    static String str;
+    static String subStr;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        subStr = br.readLine();
+
+        int ans = 0;
+        int idx = 0;
+
+        out:for(int i = 0 ; i < str.length(); ++i){
+            // subStr(부분 문자열)의 시작이 str(문자열)과 같을 때, 시작
+            if(str.charAt(i) == subStr.charAt(idx)){
+                while(true){
+
+                    // 현재까지 일치, 다음 탐색 진행
+                    i++;
+                    idx++;
+
+                    // 문자열 같음
+                    if(idx >= subStr.length()){
+                        ans = 1;
+                        break out;
+                    }
+                    // 부분문자열 탐색이 끝나기 전에 문자열의 길이 탐색이 끝났을 때 : 0
+                    if(i >= str.length() && idx < subStr.length()){
+                        break;
+                    }
+
+                    // 문자열이 다르다면 : 0
+                    if(str.charAt(i) != subStr.charAt(idx)) {
+                        idx = 0;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
---
+++ src/StepByStep/day210501/BOJ2608_로마숫자.java
@@ -0,0 +1,103 @@
+package StepByStep.day210501;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ2608_로마숫자 {
+
+    static String input;
+    static Map<Character, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 로마숫자와 아라비아 숫자 매칭 (아스키코드를 이용해서 int 배열을 넣는게 더 효율)
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        // 두 로마 숫자의 합
+        int sum = 0;
+        for (int i = 0; i < 2; ++i) {
+            sum += romanToNumber(br.readLine());
+        }
+
+        // numberToRoman(sum) = sum 을 로마 숫자로 바꿈
+        sb.append(sum).append("\n").append(numberToRoman(sum));
+        System.out.println(sb);
+    }
+
+    // 로마 숫자 -> 아라비아 숫자
+    public static int romanToNumber(String str) {
+        int result = 0;
+
+        for (int idx = 0; idx < str.length(); ++idx) {
+            int now = map.get(str.charAt(idx)); // 현재 숫자
+
+            // 현재숫자(10) < 다음 숫자(50) ==> 다음 숫자(50) - 현재숫자(10)
+            if (idx != str.length() - 1 && now < map.get(str.charAt(idx + 1))) {
+                result += map.get(str.charAt(idx + 1)) - now;
+                idx++;
+                continue;
+            }
+            result += now;
+        }
+
+        return result;
+    }
+
+    // 아라비아 숫자 -> 로마 숫자
+    public static String numberToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        while (num != 0) {
+            if (num >= 1000) {
+                sb.append("M");
+                num -= 1000;
+            } else if (num / 100 == 9) {
+                sb.append("CM");
+                num -= 900;
+            } else if (num >= 500) {
+                sb.append("D");
+                num -= 500;
+            } else if (num / 100 == 4) {
+                sb.append("CD");
+                num -= 400;
+            } else if (num >= 100) {
+                sb.append("C");
+                num -= 100;
+            } else if (num / 10 == 9) {
+                sb.append("XC");
+                num -= 90;
+            } else if (num >= 50) {
+                sb.append("L");
+                num -= 50;
+            } else if (num / 10 == 4) {
+                sb.append("XL");
+                num -= 40;
+            } else if (num >= 10) {
+                sb.append("X");
+                num -= 10;
+            } else if (num % 10 == 9) {
+                sb.append("IX");
+                num -= 9;
+            } else if (num >= 5) {
+                sb.append("V");
+                num -= 5;
+            } else if (num % 10 == 4) {
+                sb.append("IV");
+                num -= 4;
+            } else if (num >= 1) {
+                sb.append("I");
+                num -= 1;
+            }
+        }
+
+        return sb.toString();
+    }
+}
