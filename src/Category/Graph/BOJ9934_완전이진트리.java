90f38b282b3071fa46ce984ca3200eafd4a62ea5
jaehyun kim <kimjaehyun9409@gmail.com>
2021-06-20 21:00:19 +0900

    21.06.20 Tree, 완전이진트리

--- src/Category/Graph/BOJ9934_완전이진트리.java
+++ src/Category/Graph/BOJ9934_완전이진트리.java
@@ -5,59 +5,45 @@ import java.io.*;
 
 public class BOJ9934_완전이진트리 {
 
-    private static int N, idx;
-    private static int[] tree, arr;
+    private static int K, idx;
+    private static int[] arr;
+    private static int[] tree;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
-        tree = new int[(int)Math.pow(2, N)];
-        arr = new int[(int)Math.pow(2, N)];
+        K = Integer.parseInt(br.readLine());
+        arr = new int[(1<<K)];
+        tree = new int[(1<<K)];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i < tree.length; ++i){
+        for(int i = 1; i < arr.length; ++i){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        idx = 1;
+        // 풀이1. 재귀함수 이용
         dfs(1);
-        System.out.println(Arrays.toString(tree));
 
-        int j = 2;
-        for(int i = 1; i < j; ++i){
+        int depth = 1;
+        for(int i = 1; i < tree.length; ++i){
             sb.append(tree[i]).append(" ");
-            if(j == i -1){
-                i = j-1;
-                j *= 2;
+            if(i == (1<<depth) - 1){
+                depth++;
                 sb.append("\n");
             }
-            if(j > tree.length) break;
         }
+
         System.out.println(sb);
     }
 
     public static void dfs(int num){
-        if(num >= tree.length){
-            return;
-        }
+
+        if(num >= tree.length) return;
 
         dfs(num*2);
-        setTree(num, idx);
-        ++idx;
-
-        setTree(num, idx);
-        ++idx;
-
-        dfs(num*2+1);
-        setTree(num, idx);
-        ++idx;
-    }
-
-    public static void setTree(int num, int idx){
-        if(num >= tree.length || idx >= tree.length) return;
-        tree[num] = arr[idx];
+        tree[num] = arr[++idx];
+        dfs(num*2 + 1);
     }
 
 }
