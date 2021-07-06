bb0890da3c5574ea1bc4106f6192efb91206027f
김재현 <kimjaehyun9409@gmail.com>
2021-07-06 19:13:13 +0900

    21.07.06 Back-Tracking, N-Queen

--- src/Category/BackTracking/BOJ9663_N_Queen.java
+++ src/Category/BackTracking/BOJ9663_N_Queen.java
@@ -6,78 +6,89 @@ import java.io.*;
 public class BOJ9663_N_Queen {
 
     private static final int[] dy = {-1, 1, 0, 0, -1, -1, 1, 1};
-    private static final int[] dx = {0, 0, -1, 1, -1, 1, 1, -1};
+    private static final int[] dx = {0, 0, -1, 1, 1, -1, 1, -1};
 
     private static int N, ans;
     private static int[][] map;
+    private static int[][] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
 
-        permutation(0, 0, N, new int[N][N]);
+        // 입력
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];     // debugging 용 : 퀸이 놓여있는 자리를 확인
+        visited = new int[N][N]; // 퀸이 지나갈 수 있는 곳을 방문 표시
+
+        // 조합을 통하여 경우의 수 구하기
+        combination(N, 0);
+
+        // 정답 출력
         System.out.println(ans);
     }
 
-    public static void permutation(int startY, int startX, int cnt, int[][] visited) {
+    // 조합(dfs)
+    public static void combination(int cnt, int startIdx) {
+
+        // N개의 Queen 이 규칙에 맞게 놓인 경우
         if (cnt == 0) {
-            print(visited);
-            ++ans;
+            // print(); // debugging 용
+            ans++;
             return;
         }
-        if (startX == N) {
-            ++startY;
-            startX = 0;
-        }
-        for (int y = startY; y < N; ++y) {
-            for (int x = startX; x < N; ++x) {
-                if (visited[y][x] != 0) continue;
-//                System.out.println(y + " " + x);
-//                setVisited(cnt, y, x, visited);
-//                print(visited);
-                visited[y][x] = cnt;
-                permutation(y, x + 1, cnt - 1, visited);
-                visited[y][x] = 0;
-//                unSetVisited(cnt, y, x, visited);
-            }
+
+        // N*N 개 중 N 개를 선택
+        for (int i = startIdx; i < N * N; ++i) {
+            int y = i / N; // 행
+            int x = i % N; // 열
+            if(visited[y][x] != 0) continue; // 퀸이 지나갈 수 있는 곳이라면, Pass
+            setVisited(y, x, cnt); // 퀸이 놓을 수 있는 자리라면, 퀸이 지나갈 수 있는 모든 곳 방문 표시
+            combination(cnt - 1, i + 1); // 다음 퀸이 놓을 자리를 구하기
+            unSetVisited(y, x, cnt); // 다른 조합에 사용되어야하기 때문에, 이미했던 경우의 수는 방문표시를 되돌려 놓는다.
         }
     }
 
-    public static int index;
-    public static void setVisited(int cnt, int y, int x, int[][] visited) {
+    // 방문 표시 : 퀸이 지나갈 수 있는 곳을 방문 표시한다.
+    public static void setVisited(int y, int x, int num) {
+        // 8방 check
         for (int d = 0; d < 8; ++d) {
-            int ny = y, nx = x;
+            int ny = y;
+            int nx = x;
             while (isIn(ny, nx)) {
-                if (visited[ny][nx] == 0) visited[ny][nx] = cnt;
+                if (visited[ny][nx] == 0) visited[ny][nx] = num;
                 ny += dy[d];
                 nx += dx[d];
             }
         }
     }
 
-    public static void unSetVisited(int cnt, int y, int x, int[][] visited) {
+    // 방문 표시 해제 : 이미 시행한 경우의 수가 다음 경우의 수에 영향을 미치면 안되기 때문에
+    //                시행했던 경우의 수가 방문한 자리를 되돌려 놓는다(초기화 한다).
+    public static void unSetVisited(int y, int x, int num) {
         for (int d = 0; d < 8; ++d) {
-            int ny = y, nx = x;
+            int ny = y;
+            int nx = x;
             while (isIn(ny, nx)) {
-                if (visited[ny][nx] == cnt) visited[ny][nx] = 0;
+                if (visited[ny][nx] == num) visited[ny][nx] = 0;
                 ny += dy[d];
                 nx += dx[d];
             }
         }
     }
 
+    // 배열의 유효 범위 체크
     public static boolean isIn(int y, int x) {
         return y >= 0 && x >= 0 && y < N && x < N;
     }
 
-    public static void print(int[][] visited){
-        System.out.println("==========");
+    // Debugging 용 함수 (퀸이 놓여있는 자리 확인)
+    public static void print(){
         for(int y = 0; y < N; ++y){
-            for(int x = 0; x < N; ++x){
-                System.out.print(visited[y][x]);
+            for(int x = 0; x <N; ++x){
+                System.out.print(map[y][x] + " ");
             }
             System.out.println();
         }
+        System.out.println("============");
     }
 }
