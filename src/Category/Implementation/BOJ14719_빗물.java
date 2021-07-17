0e1ddda241cad680bd8d2e7808c97ee2eecb4c3c
김재현 <kimjaehyun9409@gmail.com>
2021-07-17 11:56:05 +0900

    21.07.17 greedy, 빗물

--- src/Category/Implementation/BOJ14719_빗물.java
+++ src/Category/Implementation/BOJ14719_빗물.java
@@ -8,6 +8,7 @@ public class BOJ14719_빗물 {
     private static int H, W;
     private static int[] arr;
     private static int count;
+    private static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,27 +17,39 @@ public class BOJ14719_빗물 {
         H = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
         arr = new int[W];
+        visited = new boolean[W];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < W; ++i){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         rain();
+        System.out.println(count);
     }
     // 3 4 3 3
     public static void rain(){
         int left = 0;
-        int right = 0;
-        for(int i = 1; i < W; ++i){
+        int right = W-1;
+        for(int i = 0; i < W; ++i){
             // 오름차순
             if(arr[left] <= arr[i]){
                 for(int j = left; j< i; ++j){
                     count += arr[left] - arr[j];
+                    visited[j] = true;
                 }
-                left = arr[i];
+                left = i;
             }
         }
 
-
+        for(int i = W-2; i >= 0; --i){
+            // 내림차순
+            if(arr[right] <= arr[i]){
+                for(int j = right; j > i; --j){
+                    if(visited[j]) continue;
+                    count += arr[right] - arr[j];
+                }
+                right = i;
+            }
+        }
     }
 }
