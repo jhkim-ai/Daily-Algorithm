b4d0e80953b7a5deb15f7d3cae1e3d546a2527c3
김재현 <kimjaehyun9409@gmail.com>
2021-08-12 01:12:12 +0900

    21.08.12 merge

--- src/Category/Sorting/BOJ1015_수열정렬.java
+++ src/Category/Sorting/BOJ1015_수열정렬.java
@@ -17,33 +17,34 @@ public class BOJ1015_수열정렬 {
         Element[] elements = new Element[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; ++i){
+        for (int i = 0; i < N; ++i) {
             A[i] = Integer.parseInt(st.nextToken());
             elements[i] = new Element(i, A[i]);
         }
 
         Arrays.sort(elements);
-        for(int i = 0; i < N; ++i){
+        for (int i = 0; i < N; ++i) {
             P[elements[i].index] = i;
         }
 
-        for(int p : P){
+        for (int p : P) {
             sb.append(p).append(" ");
         }
         System.out.println(sb);
     }
 
-    static class Element implements Comparable<Element>{
+    static class Element implements Comparable<Element> {
+
         int index;
         int value;
 
-        public Element(int index, int value){
+        public Element(int index, int value) {
             this.index = index;
             this.value = value;
         }
 
         @Override
-        public int compareTo(Element e){
+        public int compareTo(Element e) {
             return Integer.compare(this.value, e.value);
         }
     }
--- src/StepByStep/day210814/BOJ14500_테트로미노.java
+++ src/StepByStep/day210814/BOJ14500_테트로미노.java
@@ -1,3 +1,5 @@
+package StepByStep.day210814;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
