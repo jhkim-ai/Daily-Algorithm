b366503fac5d6069186d70d74115200ca8503928
김재현 <kimjaehyun9409@gmail.com>
2021-08-25 22:43:06 +0900

    21.08.25 삼성기출, 경사로

--- src/Main.java
+++ src/Main.java
@@ -3,142 +3,27 @@ import java.util.*;
 
 public class Main {
 
-    private static final int[] dy = {0, 0, 0, -1, 1}; // 동, 서, 북, 남
-    private static final int[] dx = {0, 1, -1, 0, 0};
-
-    private static int N, M, K;
-    private static int Y, X;
-    private static int[] commands, dice;
-    private static int[][] map;
-
-    private static StringBuilder sb;
+    private static int N, S;
+    private static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        commands = new int[K];
-        dice = new int[6]; // 0: 윗, 1: 아래, 2: 왼, 3: 오, 4: 앞, 5: 뒷
-        sb = new StringBuilder();
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
 
-        for(int y = 0; y < N; ++y){
-            st = new StringTokenizer(br.readLine());
-            for(int x = 0; x < M; ++x){
-                map[y][x] = Integer.parseInt(st.nextToken());
-            }
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; ++i){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
-        for(int k = 0; k < K; ++k){
-            commands[k] = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < N; ++i){
+            sum += arr[i];
         }
-
-        for(int command : commands){
-            int ny = Y + dy[command];
-            int nx = X + dx[command];
-            if(!isIn(ny, nx)) {
-                continue;
-            }
-            Y = ny;
-            X = nx;
-            move(command);
-        }
-        System.out.print(sb);
-    }
-
-    public static void move(int dir){
-        switch(dir){
-            case 1: // 동
-                moveEast();
-                break;
-            case 2: // 서
-                moveWest();
-                break;
-            case 3: // 북
-                moveNorth();
-                break;
-            case 4: // 남
-                moveSouth();
-                break;
-        }
-    }
-
-    public static void moveEast(){
-        // 0: 윗, 1: 아래, 2: 왼, 3: 오, 4: 앞, 5: 뒷
-        int[] tmp = new int[6];
-        tmp[0] = dice[2];
-        tmp[1] = dice[3];
-        tmp[2] = dice[1];
-        tmp[3] = dice[0];
-        tmp[4] = dice[4];
-        tmp[5] = dice[5];
-
-        dice = tmp;
-        copyBottomOfDice();
-        sb.append(dice[0]).append("\n");
-    }
-
-    public static void moveWest(){
-        // 0: 윗, 1: 아래, 2: 왼, 3: 오, 4: 앞, 5: 뒷
-        int[] tmp = new int[6];
-        tmp[0] = dice[3];
-        tmp[1] = dice[2];
-        tmp[2] = dice[0];
-        tmp[3] = dice[1];
-        tmp[4] = dice[4];
-        tmp[5] = dice[5];
-
-        dice = tmp;
-        copyBottomOfDice();
-        sb.append(dice[0]).append("\n");
-    }
-
-    public static void moveSouth(){
-        // 0: 윗, 1: 아래, 2: 왼, 3: 오, 4: 앞, 5: 뒷
-        int[] tmp = new int[6];
-        tmp[0] = dice[5];
-        tmp[1] = dice[4];
-        tmp[2] = dice[2];
-        tmp[3] = dice[3];
-        tmp[4] = dice[0];
-        tmp[5] = dice[1];
-
-        dice = tmp;
-        copyBottomOfDice();
-        sb.append(dice[0]).append("\n");
-    }
-
-    public static void moveNorth(){
-        // 0: 윗, 1: 아래, 2: 왼, 3: 오, 4: 앞, 5: 뒷
-        int[] tmp = new int[6];
-        tmp[0] = dice[4];
-        tmp[1] = dice[5];
-        tmp[2] = dice[2];
-        tmp[3] = dice[3];
-        tmp[4] = dice[1];
-        tmp[5] = dice[0];
-
-        dice = tmp;
-        copyBottomOfDice();
-        sb.append(dice[0]).append("\n");
-    }
-
-    public static void copyBottomOfDice(){
-        if(map[Y][X] == 0){
-            map[Y][X] = dice[1];
-        } else {
-            dice[1] = map[Y][X];
-            map[Y][X] = 0;
-        }
-    }
-
-    public static boolean isIn(int y, int x){
-        return y >= 0 && x >= 0 && y < N && x < M;
     }
 }
---
+++ src/SamsungSW_A/BOJ14890_경사로.java
@@ -0,0 +1,139 @@
+package SamsungSW_A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14890_경사로 {
+
+    private static int N, L, ans;
+    private static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+
+        for (int y = 0; y < N; ++y) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int x = 0; x < N; ++x) {
+                map[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; ++i) {
+            if (rowCheck(i)) {
+                ++ans;
+            }
+            if (colCheck(i)) {
+                ++ans;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    // Row 체크
+    public static boolean rowCheck(int y) {
+        int start = map[y][0];
+        int count = 1;
+        boolean goDown = false;
+
+        for (int x = 1; x < N; ++x) {
+            if (start == map[y][x]) { // 높이가 같을 때
+                ++count;
+                continue;
+            }
+
+            if (start + 1 == map[y][x]) { // 올라갈 때
+                if (!isValid(count, L)) {
+                    return false;
+                }
+                // 내려갔다 올라갔을 경우. 경사로가 겹치면 안되기에 L*2 만큼 있어야 한다.
+                if (goDown && !isValid(count, L * 2)) {
+                    return false;
+                }
+                goDown = false;
+            } else if (start - 1 == map[y][x]) { // 내려갈 때,
+                if (goDown && !isValid(count, L)) {  // 내려간 후 또 내려갈 때 이전 층에서 경사로를 설치할 수 있는지 확인
+                    return false;
+                }
+                goDown = true;
+            } else { // 2칸 이상 높거나 낮다면 pass
+                return false;
+            }
+            start = map[y][x];
+            count = 1;
+        }
+
+        if (goDown && !isValid(count, L)) { // 내려간 상태로 끝난 경우엔 check 한 후 함수 종료
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Column 체크
+    public static boolean colCheck(int x) {
+        int start = map[0][x];
+        int count = 1;
+        boolean goDown = false;
+
+        for (int y = 1; y < N; ++y) {
+            if (start == map[y][x]) { // 높이가 같을 때
+                ++count;
+                continue;
+            }
+
+            if (start + 1 == map[y][x]) { // 올라갈 때
+                if (!isValid(count, L)) {
+                    return false;
+                }
+                // 내려갔다 올라갔을 경우. 경사로가 겹치면 안되기에 L*2 만큼 있어야 한다.
+                if (goDown && !isValid(count, L * 2)) {
+                    return false;
+                }
+                goDown = false;
+            } else if (start - 1 == map[y][x]) { // 내려갈 때,
+                if (goDown && !isValid(count, L)) {  // 내려간 후 또 내려갈 때 이전 층에서 경사로를 설치할 수 있는지 확인
+                    return false;
+                }
+                goDown = true;
+            } else { // 2칸 이상 높거나 낮다면 pass
+                return false;
+            }
+            start = map[y][x];
+            count = 1;
+        }
+
+        if (goDown && !isValid(count, L)) { // 내려간 상태로 끝난 경우엔 check 한 후 함수 종료
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean isValid(int count, int minValue) {
+        return count >= minValue;
+    }
+}
+//row
+//6 2
+//3 2 2 1 1 1
+//3 2 2 2 2 1
+//3 2 2 3 3 3
+//3 2 2 2 3 3
+//3 2 2 2 2 3
+//3 3 3 3 2 2
+
+//col
+//6 2
+//3 3 3 3 3 3
+//3 2 2 2 2 2
+//3 2 2 2 2 2
+//3 2 2 3 2 1
+//3 2 3 3 2 1
+//3 3 3 3 1 1
