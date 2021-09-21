b5c19e8e24d481a5236c3c3beab6dfcbfa223185
김재현 <kimjaehyun9409@gmail.com>
2021-09-21 13:39:34 +0900

    21.09.21 simulation, 컨베이어벨트위의 로봇

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int K = Integer.parseInt(br.readLine());
-        int[] arr = new int[N*N];
+    public static void main(String[] args) {
 
-        for(int i = 0; i < arr.length; ++i){
-            int row = i / N + 1;
-            int col = i % N + 1;
-            arr[i] = row*col;
-        }
-        Arrays.sort(arr);
-        System.out.println(arr[K-1]);
     }
 }
-
-
