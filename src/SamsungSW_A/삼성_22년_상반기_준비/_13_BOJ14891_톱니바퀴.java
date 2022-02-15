e74e135376db17663d2929f97268ada96116293a
김재현 <kimjaehyun9409@gmail.com>
2022-02-15 14:17:17 +0900

    22.02.15 implementation, 성적통계

---
+++ src/SamsungSW_A/삼성_22년_상반기_준비/_13_BOJ14891_톱니바퀴.java
@@ -0,0 +1,12 @@
+package SamsungSW_A.삼성_22년_상반기_준비;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _13_BOJ14891_톱니바퀴 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
