89b53b7177c9d5e8a114c180a05e52331a43241b
김재현 <kimjaehyun9409@gmail.com>
2022-03-15 23:05:12 +0900

    22.03.15 정렬, 세 수 정렬

---
+++ src/Category/Sorting/BOJ2752_세수정렬.java
@@ -0,0 +1,22 @@
+package Category.Sorting;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ2752_세수정렬 {
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int[] arr = new int[3];
+            for(int i = 0; i < 3; ++i){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+            for(int i = 0; i < 3; ++i){
+                System.out.print(arr[i] + " ");
+            }
+    }
+}
