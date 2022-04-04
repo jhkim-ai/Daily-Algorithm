d5ff2b35978147964014c40feba8d186a9dbb5b5
김재현 <kimjaehyun9409@gmail.com>
2022-04-04 21:19:55 +0900

    사다리조작 임시 저장

---
+++ src/SamsungSW_A/삼성_22년_상반기_준비/_15_BOJ15684_사다리조작.java
@@ -0,0 +1,15 @@
+package SamsungSW_A.삼성_22년_상반기_준비;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15_BOJ15684_사다리조작 {
+
+    private static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = Integer.toString(1000, 8);
+    }
+}
