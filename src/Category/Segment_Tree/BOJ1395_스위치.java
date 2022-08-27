c81a92bc1e496f77a75f1f0911f0ff3b56611fd0
김재현 <kimjaehyun9409@gmail.com>
2022-08-27 20:22:22 +0900

    22.08.27 Segment Tree - Lazy Propagation, 중앙값

---
+++ src/Category/Segment_Tree/BOJ1395_스위치.java
@@ -0,0 +1,13 @@
+package Category.Segment_Tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1395_스위치 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
