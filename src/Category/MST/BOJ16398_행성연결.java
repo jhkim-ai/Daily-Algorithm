0dc46badd3f417da95cb7fa3ae1dfcd523a28037
김재현 <kimjaehyun9409@gmail.com>
2022-09-01 23:07:06 +0900

    22.09.01 MST, 행성연결

--- src/Category/MST/BOJ16398_행성연결.java
+++ src/Category/MST/BOJ16398_행성연결.java
@@ -1,42 +1,94 @@
 package Category.MST;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class BOJ16398_행성연결 {
 
     private static int N;
-    private static Node[] nodes;
+    private static int[] root, rank;
+    private static PriorityQueue<Edge> pq;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        nodes = new Node[N+1];
 
-        while (N-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            nodes[a] = new Node(a, b, w);
+        N = Integer.parseInt(br.readLine());
+        pq = new PriorityQueue<>();
+
+        for(int y = 0; y < N; ++y){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int x = 0; x < N; ++x){
+                int cost = Integer.parseInt(st.nextToken());
+                if(x < y) continue;
+                pq.offer(new Edge(y, x, cost));
+            }
         }
 
+        makeSet();
+
+        int cnt = 0;
+        long ans = 0;
+        while(!pq.isEmpty()){
+            Edge now = pq.poll();
+
+            if(union(now.from, now.to)){
+                ans += now.cost;
+                cnt++;
+                if(cnt == N - 1) break;
+            }
+        }
+
+        System.out.println(ans);
     }
 
-    static class Node implements Comparable<Node>{
-        int num;
-        int next;
-        int weight;
+    public static void makeSet(){
+        rank = new int[N];
+        root = new int[N];
+        for(int i = 0; i < N; ++i){
+            root[i] = i;
+        }
+    }
 
-        public Node(int num, int next, int weight) {
-            this.num = num;
-            this.next = next;
-            this.weight = weight;
+    public static int find(int a){
+        if(a == root[a]) return a;
+        return root[a] = find(root[a]);
+    }
+
+    public static boolean union(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a == b) return false;
+
+        if(rank[a] < rank[b]) {
+            root[a] = b;
+        } else {
+            root[b] = a;
+            if(rank[a] == rank[b]){
+                rank[a]++;
+            }
+        }
+
+        return true;
+    }
+
+    public static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
 
         @Override
-        public int compareTo(Node n){
-            return Integer.compare(this.weight, n.weight);
+        public int compareTo(Edge e){
+            return Integer.compare(this.cost, e.cost);
         }
     }
 }
