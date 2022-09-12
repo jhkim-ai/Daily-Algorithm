9be0304cf1d650125b00be5fa276f61bf6dcd0d4
김재현 <kimjaehyun9409@gmail.com>
2022-09-12 17:43:55 +0900

    modify code, String.format 속도 문제 개선

--- src/Category/Segment_Tree/BOJ2357_최솟값과최댓값.java
+++ src/Category/Segment_Tree/BOJ2357_최솟값과최댓값.java
@@ -7,12 +7,8 @@ import java.util.StringTokenizer;
 
 public class BOJ2357_최솟값과최댓값 {
 
-    private static final int MIN_VALUE = Integer.MIN_VALUE;
-    private static final int MAX_VALUE = Integer.MAX_VALUE;
-
     private static int N, M;
-    private static int height, cntLeafNode;
-    private static int[] arr, minTree, maxTree;
+    private static long[] arr, minTree, maxTree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,89 +17,72 @@ public class BOJ2357_최솟값과최댓값 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1];
+        arr = new long[N + 1];
 
         for(int idx = 1; idx <= N; ++idx){
-            arr[idx] = Integer.parseInt(br.readLine());
+            arr[idx] = Long.parseLong(br.readLine());
         }
 
-        setTreeInfo();
+        minTree = new long[N * 4];
+        maxTree = new long[N * 4];
 
-        makeMinTree(1, 1, cntLeafNode);
-        makeMaxTree(1, 1, cntLeafNode);
+        makeMinTree(1, 1, N);
+        makeMaxTree(1, 1, N);
 
         while(M-- > 0){
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            int minVal = getPartialMinValue(1, 1, cntLeafNode, a, b);
-            int maxVal = getPartialMaxValue(1, 1, cntLeafNode, a, b);
+            long minVal = getPartialMinValue(1, 1, N, a, b);
+            long maxVal = getPartialMaxValue(1, 1, N, a, b);
 
-            sb.append(String.format("%d %d\n", minVal, maxVal));
+            // sb.append(String.format("%d %d\n", minVal, maxVal));    // 속도: 3140 ms
+            sb.append(minVal).append(" ").append(maxVal).append("\n"); // 속도: 764 ms
         }
 
         System.out.println(sb.toString());
     }
 
-    public static int makeMaxTree(int node, int left, int right) {
-        if(left == right) {
-            if(left <= N) return maxTree[node] = arr[left];
-            else return MIN_VALUE;
-        }
+    public static long makeMaxTree(int node, int left, int right) {
+        if(left == right) return maxTree[node] = arr[left];
 
         int mid = (left + right) / 2;
-        int a = makeMaxTree(node * 2, left, mid);
-        int b = makeMaxTree(node * 2 + 1, mid + 1, right);
+        long a = makeMaxTree(node * 2, left, mid);
+        long b = makeMaxTree(node * 2 + 1, mid + 1, right);
 
         return maxTree[node] = Math.max(a, b);
     }
 
-    public static int getPartialMaxValue(int node, int left, int right, int tLeft, int tRight){
-        if(left > tRight || right < tLeft) return MIN_VALUE;
+    public static long getPartialMaxValue(int node, int left, int right, int tLeft, int tRight){
+        if(left > tRight || right < tLeft) return 0;
         if(tLeft <= left && right <= tRight) return maxTree[node];
 
         int mid = (left + right) / 2;
-        int a = getPartialMaxValue(node * 2, left, mid, tLeft, tRight);
-        int b = getPartialMaxValue(node * 2 + 1, mid + 1, right, tLeft, tRight);
+        long a = getPartialMaxValue(node * 2, left, mid, tLeft, tRight);
+        long b = getPartialMaxValue(node * 2 + 1, mid + 1, right, tLeft, tRight);
 
         return Math.max(a, b);
     }
 
-    public static int makeMinTree(int node, int left, int right) {
-        if(left == right) {
-            if(left <= N) return minTree[node] = arr[left];
-            else return MAX_VALUE;
-        }
+    public static long makeMinTree(int node, int left, int right) {
+        if(left == right) return minTree[node] = arr[left];
 
         int mid = (left + right) / 2;
-        int a = makeMinTree(node * 2, left, mid);
-        int b = makeMinTree(node * 2 + 1, mid + 1, right);
+        long a = makeMinTree(node * 2, left, mid);
+        long b = makeMinTree(node * 2 + 1, mid + 1, right);
 
         return minTree[node] = Math.min(a, b);
     }
 
-    public static int getPartialMinValue(int node, int left, int right, int tLeft, int tRight){
-        if(left > tRight || right < tLeft) return MAX_VALUE;
+    public static long getPartialMinValue(int node, int left, int right, int tLeft, int tRight){
+        if(tRight < left || right < tLeft) return 1000000001;
         if(tLeft <= left && right <= tRight) return minTree[node];
 
         int mid = (left + right) / 2;
-        int a = getPartialMinValue(node * 2, left, mid, tLeft, tRight);
-        int b = getPartialMinValue(node * 2 + 1, mid + 1, right, tLeft, tRight);
+        long a = getPartialMinValue(node * 2, left, mid, tLeft, tRight);
+        long b = getPartialMinValue(node * 2 + 1, mid + 1, right, tLeft, tRight);
 
         return Math.min(a, b);
-    }
-
-    public static void setTreeInfo() {
-        height = 1;
-        cntLeafNode = 1;
-
-        while(cntLeafNode < N) {
-            ++height;
-            cntLeafNode = cntLeafNode << 1;
-        }
-
-        minTree = new int[1 << height];
-        maxTree = new int[1 << height];
     }
 }
