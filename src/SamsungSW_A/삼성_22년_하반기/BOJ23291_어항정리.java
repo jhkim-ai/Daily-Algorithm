3c86b8d639cf25b9ff9b95eedb93f931ac9af680
jaehyun kim <kimjaehyun9409@gmail.com>
2022-10-13 20:47:07 +0900

    어항 정리 불필요한 코드 제거

--- src/SamsungSW_A/삼성_22년_하반기/BOJ23291_어항정리.java
+++ src/SamsungSW_A/삼성_22년_하반기/BOJ23291_어항정리.java
@@ -3,9 +3,7 @@ package SamsungSW_A.삼성_22년_하반기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
@@ -20,7 +18,6 @@ public class BOJ23291_어항정리 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedReader br = new BufferedReader(new StringReader(input));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
